11f998ec5a2786ef4d95e4464e4c7d22328cc6e5
CristianDLM2004 <criscrackrom14@gmail.com>
2026-02-25 13:41:27 -0600

    Habilita dom-overlay en VRButton para mostrar UI HTML en WebXR

--- src/core/VRManager.js
+++ src/core/VRManager.js
@@ -1,9 +1,17 @@
 import { VRButton } from "three/examples/jsm/webxr/VRButton.js"
 
 export class VRManager {
-
-    constructor(renderer) {
-        document.body.appendChild(VRButton.createButton(renderer))
+  constructor(renderer) {
+    const options = {
+      optionalFeatures: [
+        "local-floor",
+        "bounded-floor",
+        "hand-tracking",
+        "dom-overlay",
+      ],
+      domOverlay: { root: document.body },
     }
 
+    document.body.appendChild(VRButton.createButton(renderer, options))
+  }
 }
